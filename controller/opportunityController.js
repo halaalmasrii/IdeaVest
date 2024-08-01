1600e66d4960d7c549dfce8f7b32f2314fcaf995
Ghiath Al Akad <ghiath.al.akad@tu-clausthal.de>
2024-08-01 23:42:47 +0200

    New commit

--- controller/opportunityController.js
+++ controller/opportunityController.js
@@ -1,17 +1,22 @@
 const Opportunity = require("../models/opportunity");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const createOpportunity = async (req, res) => {
   try {
-    let {opportunityname, industry, fundingamount, reqfunding, description ,role } = req.body;
+    let {
+      opportunityname,
+      industry,
+      fundingamount,
+      reqfunding,
+      description,
+      role,
+    } = req.body;
     const userId = req.user.id;
-    if(role === "investing"){
+    if (role === "investing") {
       reqfunding = 0;
-    }
-    else if(role === "idea"){
-      fundingamount =0 
-    }
-    else{
-      return res.status(400).json({message :'Role name is not correct'})
+    } else if (role === "idea") {
+      fundingamount = 0;
+    } else {
+      return res.status(400).json({ message: "Role name is not correct" });
     }
     const opportunity = new Opportunity({
       opportunityname,
@@ -20,35 +25,44 @@ const createOpportunity = async (req, res) => {
       reqfunding,
       description,
       role,
-      user:req.user._id
+      user: req.user._id,
     });
-     const savedOp = await opportunity.save();
-   return  res.status(201).json(savedOp);
+    const savedOp = await opportunity.save();
+    return res.status(201).json(savedOp);
   } catch (err) {
-    return  res.status(400).json({ error: err.message });
+    return res.status(400).json({ error: err.message });
   }
 };
-const getOpportunity = async (req, res )=> {
+const getOpportunity = async (req, res) => {
   const role = req.params.role;
 
-
-  if(!role){
-    const op = await Opportunity.find({isDeleted: false}).populate('user')
-    return res.status(200).json(op)
+  if (!role) {
+    const op = await Opportunity.find({ isDeleted: false }).populate("user");
+    return res.status(200).json(op);
   }
-  const op = await Opportunity.find({role:role , isDeleted:false}).populate('user');
-  return res.status(200).json({op})
-}
+  const op = await Opportunity.find({ role: role, isDeleted: false }).populate(
+    "user"
+  );
+  return res.status(200).json({ op });
+};
 
 const updateOpportunity = async (req, res) => {
   try {
-    const {opportunityname, industry, fundingamount, reqfunding, description } = req.body;
+    const {
+      opportunityname,
+      industry,
+      fundingamount,
+      reqfunding,
+      description,
+    } = req.body;
     const opportunityId = req.params.id;
-    const opportunity = await opportunity.findByIdAndUpdate(
-      opportunityId,
-      { opportunityname ,industry, fundingamount, reqfunding, description },
-     
-    );
+    const opportunity = await opportunity.findByIdAndUpdate(opportunityId, {
+      opportunityname,
+      industry,
+      fundingamount,
+      reqfunding,
+      description,
+    });
     return res.json(opportunity);
   } catch (err) {
     return res.status(400).json({ error: err.message });
@@ -59,33 +73,30 @@ const softDeleteOpportunity = async (req, res) => {
   try {
     const opportunitytId = req.params.id;
     const opportunity = await Opportunity.findByIdAndUpdate({
-      _id :opportunitytId,
-     isDeleted: true ,
-  })
+      _id: opportunitytId,
+      isDeleted: true,
+    });
     console.log(opportunity);
-   return  res.status(200).json(opportunity);
+    return res.status(200).json(opportunity);
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
 };
-const getOpportunityByUser = async (req,res)=>{
+const getOpportunityByUser = async (req, res) => {
   let userId = req.params.userId;
-  console.log(userId);
-  console.log(userId);
-     let opportunity= await Opportunity.find().populate('user');
-     console.log(opportunity);
 
-     opportunity = opportunity.filter(op=>{
-      return op.user._id === id
-     })
-     res.status(200).json({opportunity})
+  let opportunity = await Opportunity.find().populate("user");
 
-}
+  const filteredOp = opportunity.filter((op) => {
+    return op.user._id.toString() === userId;
+  });
+  res.status(200).json({ filteredOp });
+};
 
 module.exports = {
   createOpportunity,
   updateOpportunity,
   softDeleteOpportunity,
   getOpportunity,
-  getOpportunityByUser
+  getOpportunityByUser,
 };
