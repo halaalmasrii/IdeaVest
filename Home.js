d08f45dccdf1095ce263977bbadfca6757f6870f
yazanalhawary <169166473+yazanalhawary@users.noreply.github.com>
2024-08-29 02:15:17 +0300

    Add files via upload
    تم رفع الواجهة الرئيسية والتنسيقات الخاصة بها

---
+++ Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+    return (
+        
+<header className="header">
+<div className="intro">
+  <h1 className="color-idea">
+    IDEA<span className="color-vest">VEST</span>
+  </h1>
+  <p className="highlight">
+    <br></br>
+    <br></br>
+    <br></br>
+    Domine o LinkedIn e alavanque sua carreira! Junte-se ao curso
+    LinkedIn Pro e descubra como aproveitar todo o potencial da maior
+    rede profissional do mundo.
+  </p>
+</div>
+<img
+  src="https://cdn.intuji.com/2022/08/Business-Technology-scaled-1.jpeg"
+  alt="Mentor"
+  className="mentor-image"
+/>
+</header>
+    
+    );
+}
+
+export default Home;
